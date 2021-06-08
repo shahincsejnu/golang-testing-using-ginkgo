6bada1a136b1b8978bdf05e5ed396a885a365952
Sahadat Hossain <sahadat_hossain@appscode.com>
2021-06-08 16:51:21 +0600

    Add separating creation and configuration: JustBeforeEach
    Signed-off-by: Sahadat Hossain <sahadat_hossain@appscode.com>

--- intro-to-BDD-in-go-using-Ginkgo/official-ginkgo/bootstrapping-a-suite/books/book_test.go
+++ intro-to-BDD-in-go-using-Ginkgo/official-ginkgo/bootstrapping-a-suite/books/book_test.go
@@ -61,6 +61,64 @@ var _ = Describe("Book", func() {
 })
 
 var _ = Describe("Book", func() {
+	var (
+		book Book
+		err  error
+		json []byte
+	)
+
+	BeforeEach(func() {
+		json = []byte(`{
+            "title":"Les Miserables",
+            "author":"Victor Hugo",
+            "pages":2783
+        }`)
+	})
+
+	JustBeforeEach(func() {
+		book, err = NewBookFromJSON2(json)
+	})
+
+	Describe("loading from JSON", func() {
+		Context("when the JSON parses succesfully", func() {
+			It("should populate the fields correctly", func() {
+				Expect(book.Title).To(Equal("Les Miserables"))
+				Expect(book.Author).To(Equal("Victor Hugo"))
+				Expect(book.Pages).To(Equal(2783))
+			})
+
+			It("should not error", func() {
+				Expect(err).NotTo(HaveOccurred())
+			})
+		})
+
+		Context("when the JSON fails to parse", func() {
+			BeforeEach(func() {
+				json = []byte(`{
+                    "title":"Les Miserables",
+                    "author":"Victor Hugo",
+                    "pages":2783oops
+                }`)
+			})
+
+			It("should return the zero-value for the book", func() {
+				Expect(book).To(BeZero())
+			})
+
+			It("should error", func() {
+				Expect(err).To(HaveOccurred())
+			})
+		})
+	})
+
+	Describe("Extracting the author's last name", func() {
+		It("should correctly identify and return the last name", func() {
+			Expect(book.AuthorLastName()).To(Equal("Hugo"))
+		})
+	})
+})
+
+var _ = Describe("Book", func() {
 	var book Book
 
 	BeforeEach(func() {
